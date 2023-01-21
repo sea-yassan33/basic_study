f67ef168d0bd23c37b6ee76ee72ec9155c4e89e1
sea-yassan33 <a.yasuda_tr51@me.com>
2023-01-21 20:45:57 +0900

    first_commit

---
+++ connecton.java
@@ -0,0 +1,7 @@
+class connecton{
+  public static void main(String[] args) {
+    System.out.println("こんにちは" + "世界");
+    System.out.println("38" + "19");
+    System.out.println(38 + 19);
+  }
+}
---
+++ numerical.java
@@ -0,0 +1,11 @@
+class numeric{
+  public static void main(String[] args) {
+    System.out.println(17);
+    System.out.println(5 + 3);
+    System.out.println("5+3");
+    System.out.println(12 / 3);
+    System.out.println(3 * 6);
+    System.out.println(8 % 3);
+    
+  }
+}
