864e943cb4576403bbe90c5a6b312d4906d991a6
sea-yassan33 <a.yasuda_tr51@me.com>
2023-01-22 08:43:24 +0900

    型変換

---
+++ type_conversion.java
@@ -0,0 +1,11 @@
+class type_conversion {
+  public static void main(String[] args) {
+    int month = 4;
+    int date = 30;
+    System.out.println(month+"月"+date+"日");
+    System.out.println(5 / 2);
+    System.out.println(5.0 / 2.0);
+    System.out.println(5 / 2.0);
+    System.out.println((double)5 / 2);
+  }
+}
