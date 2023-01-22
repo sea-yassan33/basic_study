9f740517e072be33e4e8487cb8e4adead93a2823
sea-yassan33 <a.yasuda_tr51@me.com>
2023-01-22 09:09:19 +0900

    論理演算

---
+++ logic.java
@@ -0,0 +1,9 @@
+class logic {
+  public static void main(String[] args) {
+    System.out.println(true||false);
+    System.out.println(false && true);
+    System.out.println(8 < 5 && 3 >= 2);
+    System.out.println( 8 < 5 || 3 >= 2);
+    System.out.println(!(8 < 5));
+  }  
+}
---
+++ relational.java
@@ -0,0 +1,13 @@
+class relational {
+  public static void main(String[] args) {
+    System.out.println(true);
+    System.out.println(false);
+    System.out.println(12/4 == 3);
+    System.out.println(12/4 != 3);
+    boolean bool = 3 * 9 ==27;
+    System.out.println(bool);
+    System.out.println(4 + 2 < 6);
+    System.out.println(4 + 2 >= 6);
+
+  }
+}
