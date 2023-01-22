d080b24e6d6cb6519cfe430d5cac728de0165a36
sea-yassan33 <a.yasuda_tr51@me.com>
2023-01-22 16:55:22 +0900

    配列_条件分岐

--- practice_1p.java
+++ practice_1p.java
@@ -1,5 +1,18 @@
 class practice_1p {
-  public static void main() {
-    
+  public static void main(String[] args) {
+    int[] numbers = {1, 4, 6, 9, 13, 16};
+
+    int oddSum = 0;
+    int evenSum = 0;
+
+    for(int i=0; i < numbers.length; i++){
+      if (numbers[i] % 2 == 0){
+        evenSum += numbers[i];
+      }else{
+        oddSum += numbers[i];
+      }
+    }
+    System.out.println("奇数の和は" + oddSum + "です");
+    System.out.println("偶数の和は" + evenSum + "です");
   }
 }
